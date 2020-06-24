c0c074cf12bd1734e76a57902bc7c8797f4d2f98
chanis038 <chanis038@users.noreply.github.com>
2020-06-24 16:22:46 -0600

    reparacion de clase
    +codigo borrado

--- server_GM/src/servidor_core/core.java
+++ server_GM/src/servidor_core/core.java
@@ -17,5 +17,22 @@ import java.net.URI;
  * @author usuario
  */
 public class core{
+   int port = 8080;
+    HttpServer serverMG ;
+    InetSocketAddress socket;
+    
+    public core() throws IOException{
+        socket = new InetSocketAddress(port);
+        HttpServer serverMG = HttpServer.create(socket, 0);
+        System.out.println("serverMG iniciado en el puerto" + port);
+        
+        serverMG.createContext("/", new RaizHttp());
+       //serverMG.createContext("/Header", new ControlHeaders());
+        serverMG.createContext("/credencial", new ControlPutCredencial());
+        serverMG.createContext("/message", new ControlPostMassage());
+        serverMG.createContext("/messages", new ControlGetMssgTag());
+        serverMG.setExecutor(null);
+        serverMG.start();
+        }
 
 }
